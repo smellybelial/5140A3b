477198dd7a08dbdb50b1b1c98df52c58919d28a8
Xiaotian LIU <sheldon.liuxiaotian@gmail.com>
2018-11-03 12:40:46 +1100

    add weight table view controller & camera view controller

---
+++ PetStation/CameraViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CameraViewController.swift
+//  PetStation
+//
+//  Created by Xiaotian LIU on 3/11/18.
+//  Copyright © 2018 Xiaotian LIU. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
