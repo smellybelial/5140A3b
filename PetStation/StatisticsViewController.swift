f5e482a58d0ca09e0ffb75e90a568a575daffa9f
Xiaotian LIU <sheldon.liuxiaotian@gmail.com>
2018-11-05 00:40:20 +1100

    update statistics chart view

--- PetStation/StatisticsViewController.swift
+++ PetStation/StatisticsViewController.swift
@@ -8,16 +8,55 @@
 
 import UIKit
 import Charts
+import ChartsRealm
 
-class StatisticsViewController: UIViewController {
+class StatisticsViewController: UIViewController, IAxisValueFormatter {
+    func stringForValue(_ value: Double, axis: AxisBase?) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "HH:mm.ss"
+        return dateFormatter.string(from: Date(timeIntervalSince1970: value))
+    }
+    
 
+    @IBOutlet weak var barChartView: BarChartView!
+    @IBOutlet weak var inputTextField: UITextField!
+    var visitorCounts: [(date: Date, count: Int)] = []
+    var axisFormatDelegate: IAxisValueFormatter?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.axisFormatDelegate = self
+        updateChartWithData()
     }
     
-
+    @IBAction func addValue(_ sender: Any) {
+        if let value = inputTextField.text , value != "" {
+            let date = Date()
+            let count = (NumberFormatter().number(from: value)?.intValue)!
+            self.visitorCounts.append((date, count))
+            inputTextField.text = ""
+        }
+        updateChartWithData()
+    }
+    
+    
+    func updateChartWithData() {
+        var dataEntries: [BarChartDataEntry] = []
+        for i in 0..<self.visitorCounts.count {
+            let timeIntervalForDate: TimeInterval = visitorCounts[i].date.timeIntervalSince1970
+            let dataEntry = BarChartDataEntry(x: Double(timeIntervalForDate), y: Double(visitorCounts[i].count))
+            dataEntries.append(dataEntry)
+        }
+        let chartDataSet = BarChartDataSet(values: dataEntries, label: "Visitor count")
+        let chartData = BarChartData(dataSet: chartDataSet)
+        barChartView.data = chartData
+        
+        let xaxis = barChartView.xAxis
+        xaxis.valueFormatter = axisFormatDelegate
+    }
+    
     /*
     // MARK: - Navigation
 
